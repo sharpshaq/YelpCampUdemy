b98f3eb3786d206ee03f4a07b9710a0afd45f654
Brown Fitterman <j.b.fitterman@gmail.com>
2017-06-15 16:36:32 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,65 @@
+// function isEven(num){
+//   //return true if even
+//   //return false otherwise
+//   if(num % 2 === 0){
+//     return true;
+//   } else {
+//     return(false);
+//   };
+// };
+
+// isEven(4);
+// isEven(21);
+// isEven(68);
+// isEven(333);
+
+// function isEven(num){
+//   return num % 2 === 0;
+// };
+
+// function factorial(num){
+//   //define a result variable
+//   var result = 1;
+//   //calculate factorial and store value in result
+//   for(var i = 2; i <= num; i++){
+//     result *= i;
+//   }
+//   //return result variable
+//   return result;
+// }
+
+// // factorial(4) 4 * 3 * 2 * 1
+
+// function kebabToSnake(str){
+//   //replace all "-" with "_"
+//   var newStr =  str.replace(/-/g, "_");
+//   //return str
+//   return newStr;
+// };
+
+// var friends = ["charlie", "Liz", "David", "Mattias"];
+
+// var colors = ["red", "orange", "yellow"];
+
+// array.push
+// array.pop
+// array.shift(removes first item of an array)
+// array.unshift(adds item to beginning of array)
+
+// array.indexOf("david"); finds the indexof
+// array.slice(1, 3); copies part of array. takes 2 arguments. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,154 @@
+// // var colors = ["red", "orange", "yellow", "green"];
+
+// // for(var i = 0; i <colors.length; i++){
+// //   console.log(colors[i]);
+// // }
+
+// // colors.forEach(function(){
+// //   console.log(color);
+// // });
+
+// // arr.forEach(someFunction)
+
+// // var numbers = [1, 2,3,4,5,6,7,8,9]
+
+
+// // console.log("connected");
+
+// // function printReverse(arr){
+// //   for(var i = arr.length-1; i >= 0; i++){
+// //     console.log(arr[i]);
+// //   }
+// // }
+
+// // printReverse([3,6,2,5]);
+
+// //*** isUniform() ***
+
+// // function isUniform(arr){
+// //   var first = arr[0];
+// //   for(var i = 1; i < arr.length; i++){
+// //     if(arr[i] !== first){
+// //       return false;
+// //     }
+// //   }
+// //   return true;
+// // }
+
+// // //takes array and sums all numbers up
+// // function sumArray(arr){
+// //   var total = 0;
+// //   arr.forEach(function(element){
+// //     total += element;
+// //   });
+// //   return total;
+// // }
+
+// // *** maxArray() ***
+
+// // function maxArray(arr){
+// //   var max = arr[0];
+// //   for(var i = 1; i <= arr.length; i++){
+// //     if(arr[i] > max){
+// //       max = arr[i];
+// //     }
+// //   }
+// //   return max;
+// // }
+
+// // var person = {
+// //   name: "Brown",
+// //   age: 25,
+// //   sex: "Yes PLEASE"
+// // };
+
+// // console.log(person["name"]);
+// // console.log(person.name);
+
+// var movies = [
+//   {
+//     name: "In Bruges",
+//     rating: 5,
+//     watched: true
+//   },
+//   {
+//     name: "Frozen",
+//     rating: 4.5,
+//     watched: false
+//   },
+//   {
+//     name: "Mad Max Fury Road",
+//     rating: 5,
+//     watched: true
+//   },
+//   {
+//     name: "Les Miserables",
+//     rating: 3.5, 
+//     watched: false
+//   }
+// ]
+
+// function buildString(movie){
+//   var result = "You have ";
+//   if(movie.hasWatched){
+//     result += "watched ";
+//   } else {
+//     result += "not seen ";
+//   }
+//   result += "\"" + movie.name+ "\" -";
+//   result += movie.rating + " stars";
+//   return result;
+// }
+
+// movies.forEach(function(movie){
+//   console.log(buildString(movie));
+// });
+
+// document.getElementById();
+// "gets element by ID"
+
+// document.getElementsByClassName();
+//by class
+
+// document.getElementsByTagName();
+//by all tag names
+
+// document.querySelector();
+// takes css style selector. 
+
+// document.querySelectorAll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
